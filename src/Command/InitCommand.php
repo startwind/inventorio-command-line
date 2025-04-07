95a36736e8a03fffbf5c232eab054cec4e0060b4
Nils Langner <nils.langner+webpros@webpros.com>
2025-04-07 19:33:18 +0200

    ignore

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -123,6 +123,6 @@ class InitCommand extends InventorioCommand
         $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
         $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
 
-        return self::SERVER_ID_PREFIX . vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+        return self::SERVER_ID_PREFIX . vsprintf('%s%s-%s-%s-%s-%s%s%s', /** @scrutinizer ignore-type */ str_split(bin2hex($data), 4));
     }
 }
