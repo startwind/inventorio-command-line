247ada689e5877701ea9692416ed3b908352932b
Nils Langner <nils.langner+webpros@webpros.com>
2025-04-07 13:06:09 +0200

    self update

--- bin/inventorio.php
+++ bin/inventorio.php
@@ -2,18 +2,24 @@
 
 include_once __DIR__ . '/../vendor/autoload.php';
 
+use SelfUpdate\SelfUpdateCommand;
 use Startwind\Inventorio\Command\CollectCommand;
 use Startwind\Inventorio\Command\InitCommand;
 use Symfony\Component\Console\Application;
 
 const INVENTORIO_VERSION = '##INVENTORIO_VERSION##';
+const INVENTORIO_NAME = 'Inventorio';
 
 $application = new Application();
 
 $application->setVersion(INVENTORIO_VERSION);
-$application->setName('Inventorio');
+$application->setName(INVENTORIO_NAME);
 
 $application->add(new CollectCommand());
 $application->add(new InitCommand());
+
+if (!str_contains(INVENTORIO_VERSION, '##INVENTORIO_VERSION')) {
+    $application->add(new SelfUpdateCommand(INVENTORIO_NAME, INVENTORIO_VERSION, "startwind/inventorio-command-line"));
+}
 
 $application->run();
