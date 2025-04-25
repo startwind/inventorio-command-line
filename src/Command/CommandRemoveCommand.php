4a7ac6fddc1a1c0d599bd00ac33bd08b413c4125
Nils Langner <nils.langner+webpros@webpros.com>
2025-04-25 15:28:33 +0200

    fix

--- src/Command/CommandRemoveCommand.php
+++ src/Command/CommandRemoveCommand.php
@@ -2,7 +2,7 @@
 
 namespace Startwind\Inventorio\Command;
 
-use _PHPStan_0464ac1a3\Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
