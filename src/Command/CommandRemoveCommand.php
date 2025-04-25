de7e1fae2c7fddea67b568c90a7a0f1831534f77
Nils Langner <nils.langner+webpros@webpros.com>
2025-04-25 15:44:29 +0200

    stan

--- src/Command/CommandRemoveCommand.php
+++ src/Command/CommandRemoveCommand.php
@@ -2,9 +2,9 @@
 
 namespace Startwind\Inventorio\Command;
 
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\OutputInterface;
