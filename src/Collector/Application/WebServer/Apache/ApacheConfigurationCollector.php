3b7effe96af123a26e3a902d3b629b83bd7aac41
Nils Langner <nils.langner+webpros@webpros.com>
2025-05-04 08:51:19 +0200

    apache config

--- src/Collector/Application/WebServer/Apache/ApacheConfigurationCollector.php
+++ src/Collector/Application/WebServer/Apache/ApacheConfigurationCollector.php
@@ -8,10 +8,13 @@ class ApacheConfigurationCollector extends BasicCollector
 {
     protected string $identifier = 'ApacheConfiguration';
 
+    private string $sitesEnabledPath = '/etc/apache2/sites-enabled';
+
     public function collect(): array
     {
         return [
-            'modules' => $this->getActiveApacheModules()
+            'modules' => $this->getActiveApacheModules(),
+            'nonLinkedSitesEnabled' => $this->getRealConfigFilesWithServerNames()
         ];
     }
 
@@ -35,5 +38,52 @@ class ApacheConfigurationCollector extends BasicCollector
         }
 
         return $modules;
+    }
+
+    public function isApacheInstalled(): bool
+    {
+        $output = null;
+        $returnVar = null;
+        exec('which apache2ctl || which httpd', $output, $returnVar);
+        return !empty($output);
+    }
+
+    private function getRealConfigFilesWithServerNames(): array
+    {
+        if (!$this->isApacheInstalled()) {
+            return [];
+        }
+
+        if (!is_dir($this->sitesEnabledPath)) {
+            return [];
+        }
+
+        $configFiles = scandir($this->sitesEnabledPath);
+        $results = [];
+
+        foreach ($configFiles as $file) {
+            $filePath = $this->sitesEnabledPath . DIRECTORY_SEPARATOR . $file;
+
+            if (is_file($filePath) && !is_link($filePath)) {
+                $serverName = $this->extractServerName($filePath);
+                $results[] = [
+                    'file' => $file,
+                    'serverName' => $serverName,
+                ];
+            }
+        }
+
+        return $results;
+    }
+
+    private function extractServerName(string $filePath): ?string
+    {
+        $lines = file($filePath, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (preg_match('/^\s*ServerName\s+(.+)$/i', $line, $matches)) {
+                return trim($matches[1]);
+            }
+        }
+        return null;
     }
 }
