5e1a01296f4bd845f9a9c17d6dc92c7468316cae
phmLabs <nils.langner@startwind.io>
2025-06-02 09:17:10 +0200

    minor changes

--- src/Metrics/Collector/Metric/FileLinesMetric.php
+++ src/Metrics/Collector/Metric/FileLinesMetric.php
@@ -4,6 +4,7 @@ namespace Startwind\Inventorio\Metrics\Collector\Metric;
 
 use Startwind\Inventorio\Exec\File;
 use Startwind\Inventorio\Exec\Runner;
+use Startwind\Inventorio\Metrics\Memory\Memory;
 
 abstract class FileLinesMetric implements Metric
 {
@@ -23,6 +24,12 @@ abstract class FileLinesMetric implements Metric
     public function getValue(float $lastValue): float
     {
         $lineCount = $this->getLineCount($this->filename);
+
+        // this is the first run and last value is therefore -1
+        if ($lastValue < 0) {
+            Memory::getInstance()->addData($this->getName(), $lineCount);
+            return 0;
+        }
 
         // we assume that there was a log rotation done here
         if ($lineCount < $lastValue) {
